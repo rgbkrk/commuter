7eaa60841a239a6688a36568f500e60628dd6e4d
Kyle Kelley <rgbkrk@gmail.com>
2017-03-22 15:40:18 -0700

    setup the /view route

--- packages/commuter-server/src/routes/view.js
+++ packages/commuter-server/src/routes/view.js
@@ -7,11 +7,9 @@ const router = express.Router();
 
 router.get("*", (req, res) => {
   const path = req.path;
-  log.info("on the view path");
   if (path.endsWith(".ipynb") || path.endsWith(".html") || path.endsWith("/")) {
     res.sendFile(path.resolve(__dirname, "..", "..", "build", "index.html"));
   } else {
-    // TODO: Include the config.basePath
     const newPath = path.replace(/^\/view\//, "/files/");
     res.redirect(newPath);
   }
